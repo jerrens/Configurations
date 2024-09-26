62eec8e37a0a5fa094d1811c469dac11289edd73
Saunders, Jerren C. (Att Corp.) <Jerren.Saunders@va.gov>
2024-09-26 13:57:41 -0400

    Moved Scripts and WindowsTerminal folders under new 'Windows' folder; No feature changes

--- Scripts/openssh.cmd
+++
@@ -1,3 +0,0 @@
-:: Alias for accessing OpenSSH version of ssh
-@ECHO OFF
-"C:\Windows\System32\OpenSSH\ssh.exe" %*
---
+++ Windows/Scripts/openssh.cmd
@@ -0,0 +1,3 @@
+:: Alias for accessing OpenSSH version of ssh
+@ECHO OFF
+"C:\Windows\System32\OpenSSH\ssh.exe" %*
